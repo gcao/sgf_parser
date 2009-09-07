0827c209ce9427a007dd55cf2f1247ef2236773b
Guoliang Cao <gcao@vonage.com>
2009-09-07 12:05:14 -0400

    Format parse error

--- lib/sgf/parser.rb
+++ lib/sgf/parser.rb
@@ -13,10 +13,14 @@ module SGF
       input.strip!
         
       0.upto(input.size - 1) do |i|
+        @position = i
         @stm.event(input[i,1])
       end
       
       @stm.end
+    rescue SGF::ParseError => e
+      puts e
+      raise format_error input, @position, e
     end
     
     def parse_file filename
@@ -37,6 +41,22 @@ module SGF
         parser.parse_file filename
         parser.event_listener.game
       end
+    end
+    
+    private
+    
+    def format_error input, position, e
+      if position > 1000
+        start_position = position - 1000
+        s = '"...'
+      else
+        start_position = 0
+        s = '"'
+      end
+      s << input[start_position..position]
+      s << '" <==='
+      s << e.message
+      raise s
     end
   end
 end
